015f6b9604735b8e96751b43c43eaff7a2c9a18a
shahansv <shahanvsaleem@gmail.com>
2025-11-11 21:55:29 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -18,7 +18,7 @@ const Navbar = () => {
       <nav className='px-4 pt-2 fixed w-full'>
         <div className='bg-sky-200 h-16 rounded-full px-8 flex items-center justify-between' >
           <Link to={'/'}>
-            <img src="./smartMartLogo.svg" alt="smart mart logo" className='h-1/2' />
+            <img src="./smartMartLogo.svg" alt="smart mart logo" className='h-8' />
           </Link>
           <ul className='flex  items-center'>
             <li className='font-bold text-sky-700'>
