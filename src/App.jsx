7c9d4e02230790db4cc7966e6ec668e66396e369
shahansv <shahanvsaleem@gmail.com>
2025-11-11 21:47:47 +0530

    navbar completed

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import Home from './pages/Home'
+import Navbar from './components/Navbar'
+import Inventory from './pages/Inventory'
+import Billing from './pages/Billing'
+import History from './pages/History'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/inventory' element={<Inventory />} />
+        <Route path='/billing' element={<Billing />} />
+        <Route path='/history' element={<History />} />
+      </Routes >
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import { Link } from 'react-router-dom'
+
+
+const Navbar = () => {
+  useGSAP(() => {
+    gsap.from("nav", {
+      y: -100,
+      duration: 0.7,
+      opacity: 0,
+      delay: 0.3,
+    });
+  });
+
+  return (
+    <>
+      <nav className='px-4 pt-2 fixed w-full'>
+        <div className='bg-sky-200 h-16 rounded-full px-8 flex items-center justify-between' >
+          <img src="./smartMartLogo.svg" alt="smart mart logo" className='h-1/2' />
+          <ul className='flex  items-center'>
+            <li className='font-bold text-sky-700'>
+              <Link className='navLink px-3 py-2 rounded-full' to={'/inventory'}>Inventory</Link></li>
+            <li className='font-bold text-sky-700'><Link className='navLink px-3 py-2 rounded-full' to={'/billing'}>Billing</Link></li>
+            <li className='font-bold text-sky-700'><Link className='navLink px-3 py-2 rounded-full' to={'/history'}>History</Link></li>
+          </ul>
+        </div>
+
+      </nav>
+    </>
+  )
+}
+
+export default Navbar
